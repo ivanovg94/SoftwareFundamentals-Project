d8793d1ce660f01fd5a711a5d259dbdda24e6641
ivanovg94 <georgipz94@abv.bg>
2016-12-14 14:51:32 +0200

    Initial commit

---
+++ EventSpot/EventSpot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventSpot.Startup))]
+namespace EventSpot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
