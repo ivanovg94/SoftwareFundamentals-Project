6e418b7f9fed533e4a1146f6ccd83bb26adcd2e1
StanislavaAngelova <stasiaangelova@gmail.com>
2016-12-15 20:38:46 +0200

    added delete and edit article functionality

---
+++ EventSpot/EventSpot/Models/EventViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventSpot.Models
+{
+    public class EventViewModel
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string EventName { get; set; }
+
+        [Required]
+        public string EventDate { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public string OrganizerId { get; set; }
+    }
+}
