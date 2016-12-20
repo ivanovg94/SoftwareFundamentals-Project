bb7bb477b9eb867424f9a04a566cc2af3def575d
StanislavaAngelova <stasiaangelova@gmail.com>
2016-12-20 10:32:51 +0200

    Fixed City

---
+++ EventSpot/EventSpot/Controllers/Admin/CityController.cs
@@ -0,0 +1,140 @@
+﻿using EventSpot.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventSpot.Controllers.Admin
+{
+    public class CityController : Controller
+    {
+        // GET: City
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            using (var database = new EventSpotDbContext())
+            {
+                var cities = database.Cities
+                    .ToList();
+
+                return View(cities);
+            }
+        }
+
+        //
+        // GET: City/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        //POST: City/Create
+        [HttpPost]
+        public ActionResult Create(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new EventSpotDbContext())
+                {
+                    database.Cities.Add(city);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(city);
+        }
+
+        //
+        // GET: Cityy/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new EventSpotDbContext())
+            {
+                var city = database.Cities
+                    .FirstOrDefault(c => c.Id == id);
+
+                if (city == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(city);
+            }
+        }
+        //
+        //Post: City/Edit
+        [HttpPost]
+        public ActionResult Edit(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new EventSpotDbContext())
+                {
+                    database.Entry(city).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(city);
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new EventSpotDbContext())
+            {
+                var city = database.Cities
+                    .FirstOrDefault(c => c.Id == id);
+
+                if (city == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(city);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            using (var database = new EventSpotDbContext())
+            {
+                var city = database.Cities
+                    .FirstOrDefault(c => c.Id == id);
+
+                var cityEvents = city.Events
+                    .ToList();
+
+                foreach (var events in cityEvents)
+                {
+                    database.Events.Remove(events);
+                }
+
+                database.Cities.Remove(city);
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ EventSpot/EventSpot/Models/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EventSpot.Models
+{
+    public class City
+    {
+        private ICollection<Event> events;
+
+        public City()
+        {
+            this.events =new  HashSet<Event>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Event> Events { get; set; }
+
+    }
+}
